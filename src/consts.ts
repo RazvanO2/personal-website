81b40c2b2de1fb99b552569fe82411955288fa64
Razvan Olariu <razvan.olariu@icloud.com>
2025-04-18 18:54:24 +0300

    Almost Done

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Răzvan Olariu | zvix';
+export const SITE_DESCRIPTION = 'A website about me, my projects, and my thoughts on various topics such as programming, technology, and more.';
+export const SITE_URL = 'https://zvix.ro';
